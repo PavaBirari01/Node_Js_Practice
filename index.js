29d503041fae422fa20d9300855e524cfd58f4b8
PavaBirari01 <pavanbirari01@gmail.com>
2023-04-16 00:56:43 +0530

    new branch

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-module.exports = {
-    var1 : 20,
-    var2 : 30,
-    funct : function(){
-        return 40
-    }
-};
+const ImportExport = require("./ImportExport.js");
+console.log("pavan");
+console.log("check success");
+console.log(ImportExport);
+console.log(ImportExport.var1);
+console.log(ImportExport.var2);
+console.log(ImportExport.funct());
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 7, 6, 5, 4, 4, 4, 8, 9, 0];
+console.log(arr);
+arr.filter((item) => {
+  // console.log(item);
+
+  if (item === 4) {
+    console.log(item);
+  }
+});
